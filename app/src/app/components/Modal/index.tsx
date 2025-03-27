daf8fb3cf6be7099dcb0c03108e50c394fa0aaa9
miguel <migueelcoi@gmail.com>
2025-03-26 20:17:34 -0400

    feat: modal border hover effect

--- app/src/app/components/Modal/index.tsx
+++ app/src/app/components/Modal/index.tsx
@@ -21,10 +21,13 @@ const Modal: React.FC = () => {
     if (!isOpen) return null;
     
     return ReactDOM.createPortal(
-        <div className="modal-border absolute mb-4 overflow-hidden md:rounded-lg md:p-[1px] md:pb-0 bg-white inset-0 md:top-auto md:right-auto md:left-1/2 md:bottom-0 md:w-4/5 lg:w-3/5 md:-translate-x-1/2"
-            style={{background:`radial-gradient(circle, rgba(255,255,255,0) 0%, color-mix(in srgb, ${palette.text.primary} 70%, transparent) 31%, ${palette.background.default} 100%)`}}
-        >
-                <div className="modal-content shadow-sm shadow-gray-700 h-full md:h-fit md:rounded-lg overflow-hidden"
+        <div className="modal absolute mb-4 overflow-hidden md:p-[1px] md:rounded-lg inset-0 md:top-auto md:right-auto md:left-1/2 md:bottom-0 md:w-4/5 lg:w-3/5 md:-translate-x-1/2"
+        >   
+                    
+                    <div className="modal-border fixed z-[-1] border border-white"
+                style={{background:`radial-gradient(circle, color-mix(in srgb, ${palette.text.primary} 70%, transparent) 0%, ${palette.background.default} 100%)`}}
+                     />
+                <div className=" shadow-sm shadow-gray-700 h-full md:h-fit md:rounded-lg overflow-hidden"
                     style={{
                         backgroundColor: `${[palette.background.default]}`,
                     }}
