86eb1ba328c53485b5a559e5e1f3053f340efd0b
Miguel <migueelcoi@gmail.com>
2025-12-07 13:49:25 -0400

    feat:  tab de movimentacoes

--- app/src/app/pages/movimentacoes/components/MovimentacoesDetalhes.tsx
+++ app/src/app/pages/movimentacoes/components/MovimentacoesDetalhes.tsx
@@ -19,6 +19,8 @@ import { AdicionarSalarioPopup } from "./AdicionarSalarioPopup"
 import { DetalhePopup } from "./DetalhePopup"
 import { DetalhePendentePopup } from "./DetalhePendentesPopup"
 import { InfoIcon } from "lucide-react"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs"
+
 
 export const MovimentacoesDetalhes = () => {
 
@@ -89,6 +91,8 @@ export const MovimentacoesDetalhes = () => {
         { id: 'DOZE_MESES', name: 'Últimos 12 Mêses' }
     ]
 
+    const tabTriggerClass = "data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+
     return (
 
         <div className="grid grid-cols-4 gap-4 h-full grid-rows-[min-content_1fr]">
@@ -145,31 +149,10 @@ export const MovimentacoesDetalhes = () => {
                             </div>
                             <div className="mt-6 flex flex-col ">
                                 <div className="flex justify-between p-4 ">
-                                    <div className="flex text-start flex-col w-4/5">
-                                        <div className="">Pendentes</div>
-                                        {
-                                            (periodoPendentes == 1 && pendentes.length) ?
-                                            <div className="flex text-[14px] text-center items-center gap-2 text-neutral-500">
-                                                <InfoIcon size="16"/> <span>As pendências a seguir são do próximo mês.</span>
-                                            </div> : null
-                                        }
-                                    </div>
-                                    <Button 
-                                        disabled={pendentesLoading || pendentes?.length == 0} 
-                                        onClick={processarMovimentacoesPendentes}> 
-                                        Pagar 
-                                    </Button>
+
                                 </div>
                                 <div className="flex flex-1 scroll-smooth transparent-scrollbar">
-                                    {pendentes && <DataTable.Root data={pendentes}>
-                                        <DataTable.Column description="Valor" body={valorTempl} />
-                                        <DataTable.Column description="Categoria" field="categoria" />
-                                        <DataTable.Column description="Data" body={dataTmpl} />
-                                        <DataTable.Column description="Data"
-                                            body={(row) => <DetalhePendentePopup refresh={onDesfazer} row={row} />}
 
-                                            class="flex justify-end pr-2" />
-                                    </DataTable.Root>}
                                 </div>
                             </div>
                         </div>
@@ -179,19 +162,63 @@ export const MovimentacoesDetalhes = () => {
             </div>
 
             <div className="card col-span-2 p-4 rounded-2xl flex flex-1 bg-neutral-800/40 shadow-lg overflow-y-scroll scroll-smooth transparent-scrollbar ">
+                <Tabs defaultValue="movimentacoes" className="w-full" >
+                    <TabsList className="flex gap-2 mb-4">
+                        <TabsTrigger className={tabTriggerClass} value="movimentacoes">Movimentações</TabsTrigger>
+                        <TabsTrigger className={tabTriggerClass} value="pendentes">Pendentes</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="movimentacoes">
+                        <DataTable.Root data={movimentacoes} >
+                            <DataTable.Column description="" body={tipoTempl} />
+                            <DataTable.Column description="Valor" body={valorTempl} />
+                            <DataTable.Column description="Categoria" field="categoria" />
+                            <DataTable.Column description="Data" body={dataTmpl} />
+                            <DataTable.Column description=""
+                                body={(row) => <DetalhePopup refresh={onDesfazer} row={row} />}
 
-                <DataTable.Root data={movimentacoes} >
-                    <DataTable.Column description="" body={tipoTempl} />
-                    <DataTable.Column description="Valor" body={valorTempl} />
-                    <DataTable.Column description="Categoria" field="categoria" />
-                    <DataTable.Column description="Data" body={dataTmpl} />
-                    <DataTable.Column description="Data"
-                        body={(row) => <DetalhePopup refresh={onDesfazer} row={row} />}
+                                class="flex justify-end pr-2" />
+                        </DataTable.Root>
+                    </TabsContent>
+                    <TabsContent value="pendentes">
+                        {pendentes && (
+                            <>
+                                <div className="flex mb-4">
+                                <div className="flex text-start flex-col w-4/5">
+                                    <div>Pendentes</div>
 
-                        class="flex justify-end pr-2" />
-                </DataTable.Root>
+                                    {periodoPendentes == 1 && pendentes.length ? (
+                                        <div className="flex text-[14px] text-center items-center gap-2 text-neutral-500">
+                                            <InfoIcon size="16" />
+                                            <span>As pendências a seguir são do próximo mês.</span>
+                                        </div>
+                                    ) : null}
+                                    </div>
+                                    <Button
+                                        disabled={pendentesLoading || pendentes?.length == 0}
+                                        onClick={processarMovimentacoesPendentes}
+                                    >
+                                        Pagar
+                                    </Button>
+
+                                </div>
 
 
+                                <DataTable.Root data={pendentes}>
+                                    <DataTable.Column description="Valor" body={valorTempl} />
+                                    <DataTable.Column description="Categoria" field="categoria" />
+                                    <DataTable.Column description="Data" body={dataTmpl} />
+                                    <DataTable.Column
+                                        description="Data"
+                                        body={(row) => (
+                                            <DetalhePendentePopup refresh={onDesfazer} row={row} />
+                                        )}
+                                        class="flex justify-end pr-2"
+                                    />
+                                </DataTable.Root>
+                            </>
+                        )}
+                    </TabsContent>
+                </Tabs>
             </div>
         </div>
 
